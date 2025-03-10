1b4f3379ef37e2095f47fe9ec4b1b2c1fe75bd7a
imranhossen06 <imranhossen.dev@gmail.com>
2025-03-10 23:33:35 +0600

    feat: done appbar and image assets

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,32 @@ class MyApp extends StatelessWidget{
   }
 }
 
+// class HomePage extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Colors.black,
+//      body: Center(child: Text("Imran Hossain", style: TextStyle(color: Colors.yellow),) ),
+//    );
+//   }
+
+
+// this is for image 
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
    return Scaffold(
      backgroundColor: Colors.black,
-     body: Center(child: Text("Imran Hossain", style: TextStyle(color: Colors.yellow),) ),
+     appBar: AppBar(
+       leading: Icon(Icons.home,color: Colors.white,size: 40,),
+       title: Text("Home",style: TextStyle(color: Colors.white),),
+       backgroundColor: Colors.blue,
+       elevation: 10,
+     ),
+     body: Center(
+       // child: Image.network("https://imgd.aeplcdn.com/370x208/n/cw/ec/106815/creta-exterior-right-front-three-quarter-4.jpeg?isig=0&q=80"),
+       child: Image.asset('images/img1.webp'),
+     ),
    );
   }
-  //comment
-
-
 }
