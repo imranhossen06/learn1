5e896168a1c6c9063f8ed17f57ed565c0f1d0156
imranhossen06 <imranhossen.dev@gmail.com>
2025-03-11 07:45:48 +0600

    modal richtext row column done

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,98 @@ class MyApp extends StatelessWidget{
   }
 }
 
-// class HomePage extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context) {
-//    return Scaffold(
-//      backgroundColor: Colors.black,
-//      body: Center(child: Text("Imran Hossain", style: TextStyle(color: Colors.yellow),) ),
-//    );
-//   }
 
 
-// this is for image 
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-     backgroundColor: Colors.black,
+     // backgroundColor: Colors.black,
      appBar: AppBar(
        leading: Icon(Icons.home,color: Colors.white,size: 40,),
        title: Text("Home",style: TextStyle(color: Colors.white),),
        backgroundColor: Colors.blue,
        elevation: 10,
      ),
-     body: Center(
-       // child: Image.network("https://imgd.aeplcdn.com/370x208/n/cw/ec/106815/creta-exterior-right-front-three-quarter-4.jpeg?isig=0&q=80"),
-       child: Image.asset('images/img1.webp'),
+     body: Row(
+
+       // mainAxisSize: MainAxisSize.min,
+       // spacing: 10,
+       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+       children: [
+         Row(
+           children: [
+             Column(
+               children: [
+
+                 Text("Jobayer"),
+
+                 Column(
+
+                   children: [
+
+                     Text("I am imo"),
+                     Text("from barishal"),
+                     RichText(
+                         text: TextSpan(
+                             text: "Hello ",  style: DefaultTextStyle.of(context).style,
+                             children: const <TextSpan>[
+                               TextSpan(text: 'Imo', style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black,decorationColor: Colors.red,
+                                 decorationStyle: TextDecorationStyle.wavy,),),
+                               TextSpan(text: ' world!'),
+                             ]))
+
+                   ],
+                 )
+
+
+               ],
+             ),
+             Column(
+
+               children: [
+                 Text("Rahman")
+               ],
+             ),
+             Column(
+
+               children: [
+                 Text("Rupayon"),
+               ],
+             ),
+           ],
+         ),
+
+
+
+         Row(
+mainAxisSize: MainAxisSize.max,
+           children: [
+             ElevatedButton(onPressed: () => {
+
+               showDialog(context: context, builder: (context){
+                 return AlertDialog(
+                   title: Text("Welcome"),
+                   content: Text("Welcome to the pavillion"),
+                   actions: [
+                     ElevatedButton(onPressed: ()=>{}, child: Text("Cancel"))
+                   ],
+                 );
+               })
+
+             }, child: Text("Press"))
+           ],
+         )
+
+
+       ],
+
+
+
      ),
+
+
    );
   }
 }
